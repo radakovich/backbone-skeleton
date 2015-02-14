078f6c7cbea5a0c8a4363ca96f33d2de2f239da0
Jake Radakovich <jradakovich@eventbrite.com>
2015-02-14 00:04:38 -0600

    Added Browserify and got it working.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,8 +1,12 @@
 module.exports = function(grunt){
 
     grunt.loadNpmTasks('grunt-contrib-connect');
+    grunt.loadNpmTasks('grunt-browserify');
 
     grunt.initConfig({
+        browserify: {
+            'dist/js/app.js': ['scripts/app.js']
+        },
         connect: {
             server: {
                 uses_defaults: []
